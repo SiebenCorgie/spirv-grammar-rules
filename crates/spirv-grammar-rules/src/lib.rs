bdf27ee57f205da4f81f599405834fc603ab6f6a
Tendsin Mende <tendsin@protonmail.com>
2024-04-13 23:52:35 +0200

    first round of rules (for _common_ ops).

--- crates/spirv-grammar-rules/src/lib.rs
+++ crates/spirv-grammar-rules/src/lib.rs
@@ -4,6 +4,7 @@ use serde::{Deserialize, Serialize};
 
 pub use serde;
 pub use serde_json;
+use serde_json::Value;
 
 ///Types as defined in 2.2.2 of the unified spec:
 ///<https://registry.khronos.org/SPIR-V/specs/unified1/SPIRV.html#_types>
@@ -33,22 +34,58 @@ pub enum Type {
 }
 
 #[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all = "snake_case")]
 pub enum Rule {
     ///Declares that `operand` needs to
     /// have one of `ty`'s base type.
-    #[serde(alias = "base_type")]
     BaseType {
         operand: String,
         base_types: Vec<Type>,
     },
     ///Declares that `operand` needs to have one of the
     /// concrete(?) types in `ty`.
-    #[serde(alias = "type_constrain")]
-    TypeConstraine { operand: String, ty: Vec<Type> },
+    TypeConstraint {
+        operand: String,
+        ty: Vec<Type>,
+    },
     ///Declares that the `operand` needs to have the
     ///same type as the result of this instruction.
-    #[serde(alias = "result_equal_type")]
-    ResultTypeEqual(String),
+    ResultEqualType(String),
+
+    ///The amount of components (if a / b are vectors) must be equal.
+    ComponentCountEqual {
+        a: String,
+        b: String,
+    },
+
+    ///The component width of `a` and `b` need to be equal.
+    ComponentWidthEqual {
+        a: String,
+        b: String,
+    },
+    ///The component type of `a` and `b` need to be equal
+    ComponentTypeEqual {
+        a: String,
+        b: String,
+    },
+    IsSigned {
+        operand: String,
+        is_signed: bool,
+    },
+
+    ComponentWidth {
+        operand: String,
+        allowed: Vec<usize>,
+    },
+
+    ComponentCount {
+        operand: String,
+        allowed: Vec<usize>,
+    },
+
+    ///Unknown rule that needs to be parsed at runtime.
+    #[serde(untagged)]
+    Unknown(serde_json::Value),
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -72,8 +109,14 @@ pub struct GrammarRules {
     pub rule_types: HashMap<String, String>,
     ///All defined instructions and their rules.
     pub instructions: Vec<Instruction>,
+
+    ///lookuptable, that lets us cache the intsruction lookup
+    #[serde(skip)]
+    #[serde(default)]
+    pub lookup_opcode: HashMap<u32, usize>,
 }
 
+#[derive(Debug)]
 pub enum GrammarError {
     FsError(std::io::Error),
     ParseError(serde_json::Error),
@@ -102,7 +145,36 @@ impl GrammarRules {
             source_grammar: src_grammar_file,
             rule_types,
             instructions: Vec::new(),
+            lookup_opcode: HashMap::default(),
         }
+    }
+
+    pub fn lookup_opcode(&mut self, opcode: u32) -> Option<&Instruction> {
+        let index = if let Some(known) = self.lookup_opcode.get(&opcode) {
+            *known
+        } else {
+            let idx: usize = if let Some(opcode_inst_idx) = self
+                .instructions
+                .iter()
+                .enumerate()
+                .find_map(|(instidx, inst)| {
+                    if inst.opcode == opcode {
+                        Some(instidx)
+                    } else {
+                        None
+                    }
+                }) {
+                self.lookup_opcode.insert(opcode, opcode_inst_idx);
+                opcode_inst_idx
+            } else {
+                //no inststruction for opcode :(
+                return None;
+            };
+
+            idx
+        };
+
+        self.instructions.get(index)
     }
 
     pub fn load_from_file<P: AsRef<Path>>(file: P) -> Result<Self, GrammarError> {
@@ -119,12 +191,28 @@ impl GrammarRules {
             include_str!("../../../rules/1.2/spirv.core.grammar-rules.json");
 
         //should always parse
-        serde_json::from_str(CORE_GRAMMAR_RULES).unwrap()
+        let mut rules: GrammarRules = serde_json::from_str(CORE_GRAMMAR_RULES).unwrap();
+
+        //pre populate the lookup table
+        for (instidx, inst) in rules.instructions.iter().enumerate() {
+            rules.lookup_opcode.insert(inst.opcode, instidx);
+        }
+
+        rules
     }
 
     pub fn load_glsl_std_450_grammar() -> Self {
         static GLSL_GRAMMAR_RULES: &'static str =
             include_str!("../../../rules/1.2/extinst.glsl.std.450.grammar-rules.json");
-        serde_json::from_str(GLSL_GRAMMAR_RULES).unwrap()
+
+        //should always parse
+        let mut rules: GrammarRules = serde_json::from_str(GLSL_GRAMMAR_RULES).unwrap();
+
+        //pre populate the lookup table
+        for (instidx, inst) in rules.instructions.iter().enumerate() {
+            rules.lookup_opcode.insert(inst.opcode, instidx);
+        }
+
+        rules
     }
 }
